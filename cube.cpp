4fd638a4f434a7c296abe09ede3f571305a1df44
Wizze <56793645+Wizze-exe@users.noreply.github.com>
2022-09-27 12:25:10 +0300

    Update cube.cpp

--- cube.cpp
+++ cube.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 
 using namespace std;
 
@@ -15,16 +16,21 @@ int main()
     cin >> sizeB;
     cout << "Введите ширину: ";
     cin >> sizeC;
+    cout << "\n";
     
     int array[sizeA][sizeB][sizeC];
+    string s(8*sizeA - 1, '_');
+    string s1(8*sizeA - 1, '-');
     
     for (int i = 0; i < sizeA; i++){
+        string tab(i*2, ' ');
+        cout << "block: " << i << s << endl;
         for (int j = 0; j < sizeB; j++){
             for (int k = 0; k < sizeC; k++){
                 
                 //заполнение массива случайными числами и вывод в консоль:
-                array[i][j][k] = rand() % 100;
-                cout << "\t (" << i << j << k << ") " << array[i][j][k];
+                array[i][j][k] = rand() % 90 + 10;
+                cout << "\t| " << array[i][j][k] << " |";
                 
                 //Главная диагональ:
                 if (i == j && i == k){
@@ -32,25 +38,31 @@ int main()
                 }
                 
                 //Ещё диагональ:
-                if (j + k == sizeB - 1 && i - j == 0){
+                if (j + k == sizeA - 1 && i - j == 0){
                     sumB += array[i][j][k];
                 }
                 
                 //Ещё одна диагональ:
-                if ((j == k && i != j)){
+                if (j == k && i + j == sizeA - 1){
                     sumC += array[i][j][k];
+                }
+                
+                //Последняя диагональ:
+                if (i == k && i + j == sizeA - 1){
+                    sumD += array[i][j][k];
                 }
                 
             }
             cout << endl;
         }
-        cout << endl;
+        cout << "\t" << s1 << endl;
     }
     
     //вывод диагоналей:
     cout << "Какая-то диагональ: " << sumA << endl;
     cout << "Другая диагональ: " << sumB << endl;
     cout << "Ещё одна диагональ: " << sumC << endl;
+    cout << "Последняя диагональ: " << sumD << endl;
 
     return 0;
 }
